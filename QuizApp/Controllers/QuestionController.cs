0b3a6f57914a6676a1d025848603a758a8b4e556
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-11-11 16:15:25 +0530

    Study 11/11/2022

---
+++ QuizApp/Controllers/QuestionController.cs
@@ -0,0 +1,23 @@
+﻿using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizApp.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+
+        RM_ProjectEntities db = new RM_ProjectEntities();
+        public ActionResult Index()
+        {
+            var questions = db.AptitudeQuestions.ToList();
+            if (questions == null)
+                ViewData["noQuestions"] = true;
+            return View(questions);
+        }
+    }
+}
